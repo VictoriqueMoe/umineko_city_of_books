8618f3f22c2ff5b70c515e014e060f4c3b5a62b6
Matthew Davies <mattd5125@proton.me>
2026-07-28 23:16:50 -0400

    adds original location to sign in button
    calling navigate now includes the from state used to redirect to the user's original location, giving 100x return on user experience

--- frontend/src/components/auth/LoginButton/LoginButton.tsx
+++ frontend/src/components/auth/LoginButton/LoginButton.tsx
@@ -1,11 +1,17 @@
-import { useNavigate } from "react-router";
+import { useLocation, useNavigate } from "react-router";
 import { Button } from "../../Button/Button";
 
 export function LoginButton() {
     const navigate = useNavigate();
+    const originalLocation = useLocation();
 
     return (
-        <Button variant="primary" onClick={() => navigate("/login")}>
+        <Button 
+            variant="primary" 
+            onClick={() => {
+                navigate("/login", { state: { from: originalLocation } })
+            }}
+        >
             Sign In
         </Button>
     );
